589b457245b4123b8ca800afd40695593ba3d8e1
sachushaji <sachushajiabraham@gmail.com>
2021-11-11 21:43:42 +0530

    asset frozen and asset thawed event

--- src/mappings/assets.ts
+++ src/mappings/assets.ts
@@ -53,6 +53,38 @@ export async function assetTeamChanged({
   await store.save(asset);
 }
 
+export async function assetFrozen({
+  store,
+  event,
+}: EventContext & StoreContext): Promise<void> {
+  const [asset_id] = new Assets.AssetFrozenEvent(event).params;
+  const asset = await get(store, Asset, asset_id.toString());
+  if (!asset) {
+    console.error("No asset found for id", asset_id.toString());
+    process.exit(1);
+  }
+
+  asset.status = AssetStatus.FREEZED;
+
+  await store.save(asset);
+}
+
+export async function assetThawed({
+  store,
+  event,
+}: EventContext & StoreContext): Promise<void> {
+  const [asset_id] = new Assets.AssetThawedEvent(event).params;
+  const asset = await get(store, Asset, asset_id.toString());
+  if (!asset) {
+    console.error("No asset found for id", asset_id.toString());
+    process.exit(1);
+  }
+
+  asset.status = AssetStatus.ACTIVE;
+
+  await store.save(asset);
+}
+
 export async function assetMetadata({
   store,
   event,
